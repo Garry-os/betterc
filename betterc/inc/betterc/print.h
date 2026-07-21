491a30e30b9569248d79d60a27480f10b644d53d
garry-os <baong2408@gmail.com>
2026-07-21 17:12:36 +0700

    Use fputs instead of printf

--- betterc/inc/betterc/print.h
+++ betterc/inc/betterc/print.h
@@ -2,5 +2,10 @@
 #include "format.h"
 #include <stdio.h>
 
-#define print(fmt, ...) printf("%s", format(fmt __VA_OPT__(,) __VA_ARGS__).data)
-#define println(fmt, ...) printf("%s\n", format(fmt __VA_OPT__(,) __VA_ARGS__).data)
+#define print(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stdout)
+#define println(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stdout)
+
+// Error printing
+#define eprint(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stderr)
+#define eprintln(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stderr)
+
