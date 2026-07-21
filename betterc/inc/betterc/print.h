bca34de4a012f5ad76f2212f7d6be640c4682eea
garry-os <baong2408@gmail.com>
2026-07-21 17:30:55 +0700

    Fix println

--- betterc/inc/betterc/print.h
+++ betterc/inc/betterc/print.h
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 #define print(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stdout)
-#define println(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stdout)
+#define println(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stdout); fputs("\n", stdout)
 
 // Error printing
 #define eprint(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stderr)
-#define eprintln(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stderr)
+#define eprintln(fmt, ...) fputs(format(fmt __VA_OPT__(,) __VA_ARGS__).data, stderr); fputs("\n", stderr)
 
